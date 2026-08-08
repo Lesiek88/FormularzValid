dc3d213540f9d1b070f0a964df73906b40a91a44
Lesiek88 <lesiek08@gmail.com>
2026-08-08 23:08:10 +0200

    formularz

--- app/kontakt/formularz.tsx
+++ app/kontakt/formularz.tsx
@@ -2,7 +2,44 @@
 function Formularz(){
     return (
         <div className = "App">
-            <form></form>
+            <h1>Formularz</h1> <br />
+            <form>
+                <label htmlFor="">Imie i Nazwisko:
+                    <input type="text" />
+                </label>
+                <br /><br />
+                <label htmlFor="">Firma
+                    <input type="text" />
+                </label>
+                <br /><br />
+                <label htmlFor="">NIP
+                    <input type="text" />
+                </label>
+                <br /><br />
+                <label htmlFor="">Telefon
+                    <input type="number" />
+                </label>
+                <br /><br />
+                <label htmlFor="">Budżet <br />
+                    <select name="budzet" id="budzet">
+                    <option value="p10k">Poniżej 10k</option>
+                    <option value="1015k">10-15k</option>
+                    <option value="1530k">15-30k</option>
+                    <option value="50k">50k+</option>
+                    </select>
+                </label>
+                <br /><br />
+                <label htmlFor="">Wiadomość:
+                    <input type="text" />
+                </label>
+                <br /> <br />
+                <label htmlFor="">Rodo
+                    <input type="text" />
+                </label>
+                <br /> <br />
+                <input type="submit" value="wyślij"/>
+                
+            </form>
         </div>
     )
 }
